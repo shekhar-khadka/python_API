c67f05e45b0fc55d29e2cd037a3bfe80321ecfd8
shekhar <shekharkhadka999@gmail.com>
2021-05-26 21:58:12 +0545

    first_mongo_flask_api

--- mongo_flask_api.py
+++ mongo_flask_api.py
@@ -1,89 +1,81 @@
-# Implementing the get API
-from flask import Flask, jsonify
+from flask import Flask
+from flask_pymongo import PyMongo
+
+from bson.json_util import dumps
+
+
+from flask import jsonify, request
 
 app = Flask(__name__)
 
-Nepal = [{
+app.secret_key="security"
+app.config['MONGO_URI']='mongodb://localhost:27017/flight'
 
-    "name": "province 1",
-    "capital": "biratnagar",
-    "CM": "rai",
-},
+mongo = PyMongo(app)
 
-    {
-
-        "name": "province 2",
-        "capital": "janakpur",
-        "CM": "pandit",
-    },
-
-    {
-
-        "name": "province 3",
-        "capital": "hetauda",
-        "CM": "ppp",
-    },
-
-    {
-
-        "name": "province 4",
-        "capital": "pokhara",
-        "CM": "magar",
-    }
-    ,
-    {
-
-        "name": "province 5",
-        "capital": "dang",
-        "CM": "pokhrel",
-    }
-    ,
-    {
-
-        "name": "province 6",
-        "capital": "surkhet",
-        "CM": "shai",
-    }
-
-]
-
-
-@app.route('/')
-def index():
-    return "welcome to the Nepal API"
-
-
-@app.route("/Nepal", methods=['GET'])
-def get():
-    return jsonify({'NP': Nepal})
-
-
-@app.route("/Nepal", methods=['POST'])
-def create():
-    next_state = {"name": "province 7",
-                  "capital": "dhangadi",
-                  "CM": "bhatta"}
-    Nepal.append(next_state)
-    return jsonify({'created': next_state})
+@app.route('/add',methods=['POST'])
+def add_flight():
+    _json=request.json
+    _flight_number = _json['flight_number']
+    _airlines_name= _json['airlines_name']
+    _source= _json['source']
+    _destination=_json['destination']
+    _duration=_json['duration']
 
 
 
-@app.route("/Nepal/<string:CM>",methods=['PUT'])
+    #_hashed_password=generate_password_hash(_number)
+    mongo.db.flight_data.insert({'airlines_name':_airlines_name,
+                                 'flight_number':_flight_number,
+                                        'source':_source,
+                                        'destination':_destination,
+                                        'duration':_duration
+                                 })
 
-def state_update(CM):
-    Nepal['CM']['name']="Bagmati"
-    return jsonify({"new":Nepal[CM]})
+    resp=jsonify('added successfully')
+    resp.status_code=200
+    return resp
 
 
+@app.route('/find',methods=['GET'])
 
-@app.route("/Nepal/<CM:string>",methods=['Delete'])
-def delete (CM):
-    Nepal.remove(Nepal[CM])
-    return jsonify({'result':True})
+def get_flight_status():
+    status=mongo.db.flight_data.find()
+    resp=dumps(status)
+    return resp
+
+#get flight by flight_number
+
+@app.route('/find/<number>',methods=['GET'])
+
+def get_flight_by_number(number):
+    status=mongo.db.flight_data.find({'flight_number':number})
+    resp=dumps(status)
+    return resp
+
+@app.route('/delete/<number>',methods=['DELETE'])
+
+def delete_flight_by_number(number):
+    mongo.db.flight_data.delete_one({'flight_number':number})
+    resp=jsonify("flight deleted")
+    resp.status_code=200
+    return resp
 
 
+#changing the flight number which matches the flight_time
+@app.route('/update/<flight_time>',methods=['PUT'])
+
+def update_flight(flight_time):
+    _json=request.json
+    _flight_number = _json['flight_number']
+
+    mongo.db.flight_data.update_one({"duration":flight_time},{"$set":{"flight_number":_flight_number}})
+    resp=jsonify("flight updated")
+    resp.status_code=200
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
